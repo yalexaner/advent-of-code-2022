e7882ed1a0ff5cf9d4c181837917c80d2855b056
Alexander Yachmenev <nt1qbpb2@duck.com>
2022-12-13 19:43:32 +0000

    refactoring

--- src/Day01.kt
+++ src/Day01.kt
@@ -3,55 +3,61 @@
 import kotlin.math.max
 
 fun main() {
-    fun part1(input: List<String>): Int {
-        var maxCalories = 0
-        var currentElfCalories = 0
-
-        for (line in input) {
-            if (line.isEmpty()) {
-                maxCalories = maxOf(maxCalories, currentElfCalories)
-                currentElfCalories = 0
-                continue
-            }
-
-            currentElfCalories += line.toInt()
-        }
-
-        if (currentElfCalories != 0) {
-            maxCalories = maxOf(maxCalories, currentElfCalories)
-        }
-
-        return maxCalories
-    }
-
-    fun part2(input: List<String>): Int {
-        val topElvesCalories = Array(3) { 0 }
-        var currentElfCalories = 0
-
-        for (line in input) {
-            if (line.isEmpty()) {
-                topElvesCalories.addOrReplaceIfPossible(currentElfCalories)
-                currentElfCalories = 0
-                continue
-            }
-
-            currentElfCalories += line.toInt()
-        }
-
-        if (currentElfCalories != 0) {
-            topElvesCalories.addOrReplaceIfPossible(currentElfCalories)
-        }
-
-        return topElvesCalories.sum()
-    }
-
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 24000)
-    check(part2(testInput) == 45000)
+    runTests()
 
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
+}
+
+fun runTests() {
+    val testInput = readInput("Day01_test")
+    val part1Result = part1(testInput)
+    check(part1Result == 24000) { "part1 is $part1Result, but expected 24000" }
+    val part2Result = part2(testInput)
+    check(part2Result == 45000) { "part2 is $part2Result, but exprected 45000"}
+}
+
+fun part1(input: List<String>): Int {
+    var maxCalories = 0
+    var currentElfCalories = 0
+
+    for (line in input) {
+        if (line.isEmpty()) {
+            maxCalories = maxOf(maxCalories, currentElfCalories)
+            currentElfCalories = 0
+            continue
+        }
+
+        currentElfCalories += line.toInt()
+    }
+
+    if (currentElfCalories != 0) {
+        maxCalories = maxOf(maxCalories, currentElfCalories)
+    }
+
+    return maxCalories
+}
+
+fun part2(input: List<String>): Int {
+    val topElvesCalories = Array(3) { 0 }
+    var currentElfCalories = 0
+
+    for (line in input) {
+        if (line.isEmpty()) {
+            topElvesCalories.addOrReplaceIfPossible(currentElfCalories)
+            currentElfCalories = 0
+            continue
+        }
+
+        currentElfCalories += line.toInt()
+    }
+
+    if (currentElfCalories != 0) {
+        topElvesCalories.addOrReplaceIfPossible(currentElfCalories)
+    }
+
+    return topElvesCalories.sum()
 }
 
 fun Array<Int>.addOrReplaceIfPossible(num: Int) {
